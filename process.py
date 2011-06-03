59b1d3802661c3a5cc438f5741641384d63d405a
Adam Kariv <adam.kariv@gmail.com>
2011-06-03 18:08:40 +0300

    ++

--- process.py
+++ process.py
@@ -5,19 +5,27 @@ from tok import Token
 
 PROCESSORS = [ "DataLoader",
                "PermissionChecker",
+               "CacheGetter",
                "DBOperation",
                "MultiLang",
                "ReferenceFetcher",
+               "CacheSetter",
                "DataFormatter" ]
 
 def process(path,slug):
     token = Token(request,path,slug)
     
+    to_skip = None
     for name in PROCESSORS:
+        if to_skip != None:
+            if name == to_skip:
+                to_skip = None
+            else:
+                continue
         P = Processor.get_processor(name)
         processor = P(token) 
         processor.process()
         if processor.stop():
-            break
+            to_skip = processor.skip()
     
     return token.response, token.content_type, token.headers
