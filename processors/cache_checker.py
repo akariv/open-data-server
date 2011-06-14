3a997c9cbe0bc262a6f3e19012c7d44dbe863385
Adam Kariv <adam.kariv@gmail.com>
2011-06-14 13:31:28 +0300

    goto memcache

--- processors/cache_checker.py
+++ processors/cache_checker.py
@@ -1,25 +1,30 @@
 import json
-cache = {}
+from werkzeug.contrib.cache import SimpleCache as CacheEngine
+#from werkzeug.contrib.cache import MemcachedCache as CacheEngine
+
+cache = CacheEngine()
 
 def clear_cache(key):
-    if key in cache.keys():
-        del cache[key]
+    cache.delete(key)
 
 def clear_cache_for_path(path):
-    print "clearing cache for path: %s" % path
-    for key in cache.keys():
-        if key.startswith('"%s"' % path):
-            print "clearing cache key: %s" % key
-            del cache[key]
+    pass
+#    print "clearing cache for path: %s" % path
+#    for key in cache.keys():
+#        if key.startswith('"%s"' % path):
+#            print "clearing cache key: %s" % key
+#            del cache[key]
 
 def hit_cache(key):
-    key = json.dumps(key)
-    value = cache.get(key)
-    if value != None:
-        return json.loads(value)
-    else:
-        return None
+    return cache.get(key)
+#    key = json.dumps(key)
+#    value = cache.get(key)
+#    if value != None:
+#        return json.loads(value)
+#    else:
+#        return None
 
 def store_in_cache(key,value):
-    key = json.dumps(key)
-    cache[key] = json.dumps(value)
+    cache.set(key,value,60)
+#    key = json.dumps(key)
+#    cache[key] = json.dumps(value)
