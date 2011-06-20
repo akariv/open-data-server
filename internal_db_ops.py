c97487908ef3754b2b3c5c1ab0d8c647f0f7cf4d
Adam Kariv <adam.kariv@gmail.com>
2011-06-20 20:54:00 +0300

    fix bugs in internal_db_ops

--- internal_db_ops.py
+++ internal_db_ops.py
@@ -7,9 +7,11 @@ from log import L
 def internal_save(url,obj):
     data = json.dumps(obj)
     data = g.app.test_client().post(url,data=data,content_type="application/json")
+    return data
 
 def internal_find(url,follow=True,lang=None,query=None,apikey=None,fields=None):
-    params = {}
+    params = { 'o' : 'json',
+               'limit' : 0 }
     params['follow'] = "yes" if follow else "no"
     if lang:    params['lang'] = lang
     if query:   params['query'] = json.dumps(query)
