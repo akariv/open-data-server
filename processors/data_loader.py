32c0a75bb72513e144d80e5dce8354056cc613a0
Adam Kariv <adam.kariv@gmail.com>
2011-07-09 15:20:06 +0300

    process form submit as well

--- processors/data_loader.py
+++ processors/data_loader.py
@@ -12,5 +12,8 @@ class DataLoader(Processor):
     def process(self):
         
         if self.token.request.method in ['PUT', 'POST' ]:
-            self.token.data = json.loads(self.token.data)
+            try:
+                self.token.data = json.loads(self.token.data)
+            except:
+                self.token.data = dict( [ (k,v) for k,v in self.token.request.form.iteritems() ] )
             L.debug("DataLoader:: data=%r" % self.token.data)
